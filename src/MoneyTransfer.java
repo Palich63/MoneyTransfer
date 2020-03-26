ae5f4242530f89ea45d8557de78a817c1f34ea27
Andrey Globa <Globa.Andrey@gmail.com>
2020-03-26 21:50:20 +0300

    Added Report and programm

--- src/MoneyTransfer.java
+++ src/MoneyTransfer.java
@@ -1,4 +1,18 @@
-package PACKAGE_NAME;
-
 public class MoneyTransfer {
+    public static void main(String[] args) {
+
+        int account = 2_000_000_000; // текущий баланс счёта клиента
+        System.out.println("Account: " + account);
+        int transfer = 500_000_000; // сумма перевода
+        System.out.println("Transfer: " + transfer);
+        account = account + transfer;
+        System.out.println("Total: " + account); // Состояние счёта после перевода
+
+       // Ниже код программы с новым типом данных
+        //float account = 2_000_000_000F; // текущий баланс счёта клиента
+        //System.out.println("Account: " + String.format("%.2f", account));
+       // float transfer = 500_000_000F; // сумма перевода
+        //System.out.println("Transfer: " + String.format("%.2f", transfer));
+        //System.out.println("Total: " + String.format("%.2f", account + transfer)); // Состояние счёта после перевода
+    }
 }
